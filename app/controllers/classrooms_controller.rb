fd50d4df5497f420af7a1921f30e4d51cf0d6a18
Veronica Bustabad <vbustabad@gmail.com>
2018-08-15 23:48:51 +0000

    automatically backed up by learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -3,10 +3,6 @@ require 'pry'
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-
-    @classroom.students.collect do |student|
-      @student
-    end
   end
 
   def index
